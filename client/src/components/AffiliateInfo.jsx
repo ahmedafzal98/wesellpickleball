8688dbb393ef1bedb186193d6e81fc6c437257df
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-05-06 04:38:53 +0500

    icon updated

--- client/src/components/AffiliateInfo.jsx
+++ client/src/components/AffiliateInfo.jsx
@@ -28,7 +28,7 @@ const AffiliateInfo = () => {
         <img
           src={affiliateIcon}
           alt="Affiliate Icon"
-          className="w-14 h-14 sm:w-16 sm:h-16 mx-auto my-2"
+          className="w-14 h-14 mx-auto my-2"
         />
 
         <p className="text-base sm:text-lg font-semibold text-lime-400">
