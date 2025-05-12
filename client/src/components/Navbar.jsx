aa314cb9d3a13ece01b207a94468ed12af39f1a8
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-05-13 00:34:10 +0500

    fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,14 +4,13 @@ const Navbar = () => {
   return (
     <>
       <nav className="overflow-x-hidden text-white flex flex-col items-center justify-center h-auto">
-        <div className="w-full sm:w-4/5 flex justify-center lg:justify-start">
+        <div className="w-full sm:w-4/5 flex justify-center xl:justify-start">
           <img
             src={pickleball_logo}
             alt="Pickleball Logo"
             className="h-32 sm:h-36 md:h-36 lg:h-32 xl:h-32 w-auto bg-transparent"
           />
         </div>
-        {/* Horizontal Line with added margin */}
         <div className="h-0.5 w-full bg-[#B7D92D] mt-7"></div>
       </nav>
     </>
