1dd196ad718b51277ae858bb8f5d31d9102590c8
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-09-19 02:33:53 +0500

    add origin

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 // Allowed CORS origins
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:3000",
   "https://wesellpickleball-client.onrender.com",
   "https://wesellpickleball.com",
 ];
